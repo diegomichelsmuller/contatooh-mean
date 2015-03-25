f5ea0b1d5d4d44e9ea0508f149e977c7c9f18116
Diego Michels Muller <diegomuller@DiegoMB.local>
2015-03-25 20:30:27 -0300

    sending to OpenShift

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var config = require('./config/config')();
 require('./config/passport')();
 require('./config/database')(config.db);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express Server escutando na porta ' + 
-              app.get('port'));
+http.createServer(app).listen(config.port, function(){
+  console.log('Express Server ' + config.address +  ' (' + config.env 
+              + ' )' + ' escutando na porta ' + config.port);
 });
